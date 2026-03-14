fcb894d1afe40b1bd67d202846eec903e5bdd33a
BIKASH TALUKDER <bikashtalukder040@gmail.com>
2026-03-15 05:05:40 +0600

    digital clock with alarm snooze system.c

--- digital clock.c
+++ digital clock.c
@@ -1,82 +1,136 @@
-// Digital Clock with Date, Day & Month Name (12-hour format)
 #include <stdio.h>
 #include <time.h>
-#ifdef _WIN32
-#include <windows.h>
-#else
+#include <stdlib.h>
 #include <unistd.h>
-#endif
+#include <pthread.h>
+
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+#define BOLD    "\033[1m"
+
+int alarmH, alarmM;
+int isSnoozed = 0;
+int alarmTriggered = 0;
+time_t snoozeUntil = 0;
+pthread_mutex_t alarm_mutex;
+
+static const char *months[] = {
+    "Jan","Feb","Mar","Apr","May","Jun",
+    "Jul","Aug","Sep","Oct","Nov","Dec"
+};
+
+void* clock_thread(void* arg) 
+{
+    while (1) 
+    {
+        time_t now = time(NULL);
+        struct tm *t = localtime(&now);
+
+        system("clear");
+
+        pthread_mutex_lock(&alarm_mutex);
+        int current_snooze_status = isSnoozed;
+        time_t current_snooze_until = snoozeUntil;
+        pthread_mutex_unlock(&alarm_mutex);
+
+        int displayHour = t->tm_hour;
+        char *period = (displayHour >= 12) ? "PM" : "AM";
+        if (displayHour == 0) displayHour = 12;
+        else if (displayHour > 12) displayHour -= 12;
+
+        printf(CYAN "=======================================\n" RESET);
+        printf(BOLD MAGENTA "       MAC PRO DIGITAL CLOCK\n" RESET);
+        printf(CYAN "=======================================\n" RESET);
+
+        printf(YELLOW " [Alarm: %02d:%02d]  [Snooze: %s]\n" RESET,
+               alarmH, alarmM,
+               current_snooze_status ? GREEN "ACTIVE" RESET : RED "OFF" RESET);
+
+        printf(CYAN "---------------------------------------\n" RESET);
+
+        printf(BOLD GREEN "    TIME : %02d:%02d:%02d %s\n" RESET,
+               displayHour, t->tm_min, t->tm_sec, period);
+
+        printf(BLUE "    DATE : %02d %s %04d\n" RESET,
+               t->tm_mday, months[t->tm_mon], t->tm_year + 1900);
+
+        printf(CYAN "---------------------------------------\n" RESET);
+        printf(YELLOW " >> Press 's' + Enter to Snooze\n" RESET);
+        printf(CYAN "=======================================\n" RESET);
+
+        int should_ring = 0;
+
+        if (current_snooze_status && now >= current_snooze_until) 
+        {
+            pthread_mutex_lock(&alarm_mutex);
+            isSnoozed = 0;
+            pthread_mutex_unlock(&alarm_mutex);
+            should_ring = 1;
+        } 
+        else if (!current_snooze_status &&
+                   t->tm_hour == alarmH &&
+                   t->tm_min == alarmM &&
+                   t->tm_sec == 0) 
+        {
+            should_ring = 1;
+        }
+
+        if (should_ring)
+        {
+            printf(BOLD RED "\n!!! ALARM IS RINGING !!!\a\n" RESET);
+            fflush(stdout);
+        }
+
+        sleep(1);
+    }
+    return NULL;
+}
+
+void* input_thread(void* arg) 
+{
+    char ch;
+    while (1)
+        {
+        if (scanf(" %c", &ch) == 1) 
+        {
+            if (ch == 's' || ch == 'S')
+            {
+                pthread_mutex_lock(&alarm_mutex);
+                isSnoozed = 1;
+                snoozeUntil = time(NULL) + 10;
+                pthread_mutex_unlock(&alarm_mutex);
+            }
+        }
+    }
+    return NULL;
+}
 
 int main() 
 {
-    while (1)
-     {
-        time_t now = time(NULL);
-        struct tm *t = localtime(&now);
-        int hour = t->tm_hour;
-        char *period;
+    pthread_mutex_init(&alarm_mutex, NULL);
 
-        // 12-hour conversion
-        if (hour == 0) 
-        {
-            hour = 12;
-            period = "AM";
-        }
-        else if (hour < 12)
-        {
-            period = "AM";
-        }
-        else if (hour == 12) 
-        {
-            period = "PM";
-        }
-        else 
-        {
-            hour = hour - 12;
-            period = "PM";
-        }
-        // Day names
-        char *days[] = 
-        {
-            "Sunday", "Monday", "Tuesday",
-            "Wednesday", "Thursday", "Friday", "Saturday"
-        };
+    printf(BOLD GREEN "Welcome to Pro-Clock Setup\n" RESET);
+    printf("Set Alarm (HH MM): ");
+    if (scanf("%d %d", &alarmH, &alarmM) != 2) return 1;
 
-        // Month names
-        char *months[] = 
-        {
-            "January", "February", "March",
-            "April", "May", "June",
-            "July", "August", "September",
-            "October", "November", "December"
-        };
-
-        // Clear screen
-        #ifdef _WIN32
-        system("cls");
-        #else
-        system("clear");
-        #endif
-        printf("Digital Clock with Full Date\n");
-        printf("-------------------------------\n");
-        printf("Time : %02d:%02d:%02d %s\n",
-               hour,
-               t->tm_min,
-               t->tm_sec,
-               period);
-        printf("Date : %02d %s %04d\n",
-               t->tm_mday,
-               months[t->tm_mon],  
-               t->tm_year + 1900);
-
-        printf("Day  : %s\n",
-               days[t->tm_wday]);
-
-        #ifdef _WIN32
-        Sleep(1000);
-        #else
-        sleep(1);
-        #endif
+    if (alarmH < 0 || alarmH > 23 || alarmM < 0 || alarmM > 59) 
+    {
+        printf(RED "Invalid alarm time. Use HH (0-23) and MM (0-59).\n" RESET);
+        return 1;
     }
+
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, clock_thread, NULL);
+    pthread_create(&t2, NULL, input_thread, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    pthread_mutex_destroy(&alarm_mutex);
     return 0;
 }
